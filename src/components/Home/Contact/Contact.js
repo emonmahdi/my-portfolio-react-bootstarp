b151980d8ceac0cbece18e2f2743f79927da5c26
emonmahdi <developeremon1997@gmail.com>
2023-10-30 16:52:27 +0600

    blog section and emailjs [DONE]

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import './Contact.css'
-import { FaFacebookF, FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
+import "./Contact.css";
+import {
+  FaFacebookF,
+  FaLinkedin,
+  FaGithub,
+  FaEnvelope,
+  FaPhone,
+} from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_macvirw",
+        "template_s3ipse9",
+        form.current,
+        "GYGft4Pf0HT4r9pwC"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon: "success",
+            title: "Success!",
+            text: "Your email has been sent successfully.",
+          });
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div id="contact-section">
       <hr />
       <Container>
         <div class="section-title">
           <h2>
-          GET IN <span> TOUCH</span>{" "}
+            GET IN <span> TOUCH</span>{" "}
           </h2>
         </div>
         <Row>
@@ -29,7 +65,6 @@ const Contact = () => {
                     <h5>Mail ME </h5>
                     <span>emonhowlader1997@gmail.com</span>
                   </div>
-                  
                 </div>
                 <div class="contact-one">
                   <FaPhone />
@@ -37,7 +72,6 @@ const Contact = () => {
                     <h5>Call ME </h5>
                     <span>01908131513</span>
                   </div>
-                  
                 </div>
                 <div class="social-link">
                   <a href="https://www.facebook.com/mdemon.mahdi7700/">
@@ -48,25 +82,38 @@ const Contact = () => {
                   </a>
                   <a href="https://github.com/emonmahdi">
                     <FaGithub />
-                  </a> 
+                  </a>
                 </div>
               </div>
             </div>
           </Col>
           <Col xs={12} md={8}>
-              <div className="contact-form">
-              <form action="">
-				 		<div class="name-box">  
-                            <input type="text" className='form-control mb-4' placeholder="Your Name" />  
-                            <input type="text" className='form-control mb-4' placeholder="Your Email" />  
-                            <input type="text" className='form-control mb-4' placeholder="Your Subject" />   
-				 		</div>
-				 		<textarea className='form-control mb-4' placeholder="Your Message"></textarea>
-				 		<div class="submit-btn"> 
-                            <input type="submit" value="Send Message" /> 
-						</div>
-				 	</form>
-              </div>
+            <div className="contact-form">
+              <form ref={form} onSubmit={sendEmail}>
+                <div class="name-box">
+                  <input
+                    type="text"
+                    name="from_name"
+                    className="form-control mb-4"
+                    placeholder="Your Name"
+                  />
+                  <input
+                    type="text"
+                    name="from_email"
+                    className="form-control mb-4"
+                    placeholder="Your Email"
+                  />
+                </div>
+                <textarea
+                  className="form-control mb-4"
+                  placeholder="Your Message"
+                  name="message"
+                ></textarea>
+                <div class="submit-btn">
+                  <input type="submit" value="Send Message" />
+                </div>
+              </form>
+            </div>
           </Col>
         </Row>
       </Container>
